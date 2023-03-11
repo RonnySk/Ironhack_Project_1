5d8be66693af2d2051a12852cb1f76e8b579c96e
RonnySk <ronny_sk@hotmail.com>
2023-03-11 12:21:53 +0100

    Collision teste

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let imgCar;
+let imgPoliceCar;
+let car;
+let colliding = false;
+// let PoliceCar;
 
 function preload(){
     imgCar = loadImage('/images/car.jpg');
+    imgPoliceCar = loadImage('/images/policeCar.jpg')
 }
 
 class Building {
@@ -19,52 +24,58 @@ class Building {
 
 let buildings = [
 new Building(60,60,300,50),
-new Building(420,60,345, 160)
+new Building(420,60,345, 160),
+new Building(830,60,110,160),
+new Building(60,170,300,50),
 ];  
 
 
 class Car {
-    constructor(position, velocity) {
-        this.position = position;
-        this.velocity = velocity;
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.vx = this.x;
+        this.vy = this.y;
         this.w = 50;
         this.h = 50
     }
 
     draw(){
-     image(imgCar, this.position.x, this.position.y,this.w ,this.h,0 ,0);
+     image(imgCar, this.x, this.y,this.w ,this.h,0 ,0);
     }
 }
+// class PoliceCar {
+//     constructor() {
+//         this.x = 0;
+//         this.y = 595;
+//         this.vx = this.x;
+//         this.vy = this.y;
+//         this.w = 50;
+//         this.h = 50
+//     }
 
-let car = new Car(
-    {position: {
-        x: 0,
-        y: 0
-    },
-    velocity: {
-        x: 0,
-        y: 0
-    } 
- }
-);
-
+//     draw(){
+//      image(imgPoliceCar, this.x, this.y,this.w ,this.h,0 ,0);
+//     }
+// }
 
 
 function setup(){
     createCanvas(1000, 650);
-    
+    car = new Car(); 
+    // PoliceCar = new PoliceCar();  
 }
 
 function draw(){
-    background(50)
-    keyPressed() 
-    car.draw()
-    // buildingMap.draw()
+    background(50);
+    keyPressed(); 
+    car.draw();
+
     buildings.forEach((building) => {
         building.draw()
         collisionDetection(car,building)
-    })
-    // makeBuildings()
+    });
+    
 }
 
 
@@ -78,6 +89,33 @@ function collisionDetection(rect1, rect2) {
         rect1.x + rect1.w > rect2.x &&
         rect1.y < rect2.y + rect2.h &&
         rect1.h + rect1.y > rect2.y
-        )
-           console.log("batendo porra")
+        ) {
+            console.log("collision test")
+            colliding = true
+        } else {
+            colliding = false
+        }
+    
+        // if( rect1.y < rect2.y + rect2.h) {
+        //         rect1.y = rect2.y + rect2.h;
+        //         }
+            
+        //     if( rect1.y + rect1.height > rect2.y ){ 
+        //             rect1.y = rect2.y - rect1.height
+        //         }
+    
 }
+
+
+
+// trying 2, another Example, not working
+
+// trying 3...
+
+        // if (rect1.y - rect1.w + rect1.vy <= rect2.y + rect2.h &&
+        //     rect1.x - rect1.w + rect1.vx >= rect2.x &&
+        //     rect1.y + rect1.w + rect1.vy >= rect2.y &&
+        //     rect1.x - rect1.w + rect1.x <= rect2.x + rect2.w) {
+        //         rect1.vx = 0;
+        //         rect1.y = 0
+        //     }
--- keyControls.js
+++ keyControls.js
@@ -1,17 +1,27 @@
 function keyPressed(){
-    if (keyIsDown(LEFT_ARROW)) {
+    if (keyIsDown(LEFT_ARROW) && car.x >= 5) {
+      let futureCar = {x: car.x - 5, y: car.y};
+      let didCollide = false;
+      buildings.forEach((building) => {
+         if(collisionDetection(futureCar,building)) {
+            didCollide = true
+         }
+    });  
+      if(!didCollide) {
         car.x -= 5;
+      }
+
     } 
     
-    if (keyIsDown(RIGHT_ARROW)) {
+    if (keyIsDown(RIGHT_ARROW) && car.x <= 945 && colliding === false)  {
         car.x += 5;
     }
 
-    if (keyIsDown(UP_ARROW)) {
+    if (keyIsDown(UP_ARROW) && car.y >= 5 && colliding === false) {
         car.y -= 5;
     } 
     
-    if (keyIsDown(DOWN_ARROW)) {
+    if (keyIsDown(DOWN_ARROW) && car.y <= 595 && colliding === false) {
         car.y += 5;
     }
 }
